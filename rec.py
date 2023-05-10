01a06c77b0b9255df901a15c87cab64f683f1fcb
chin5oon <117053996+chin5oon@users.noreply.github.com>
2023-05-10 23:08:20 +0800

    Add files via upload

---
+++ rec.py
@@ -0,0 +1,31 @@
+# Author: Kwa Chin Soon
+# version: 2.0
+# Nov 2022
+
+import streamlit as st
+# other libs
+import pandas as pd
+
+
+# -- Set page config
+apptitle = 'Justin-Time Periodic Inspection'
+
+st.set_page_config(page_title=apptitle, page_icon='random', layout= 'wide')
+# random icons in the browser tab
+
+st.title('Justin-Time Periodic Inspection')
+
+
+ ######################## section-1 ##################
+ # Let's add a sub-title
+st.write("Identifying similar at-risk buildings")
+
+df1 = pd.read_csv('rec.csv') 
+
+T7 = st.text_input("Enter Postal Code of Building that has been labelled T7:", "")
+
+st.write('The building address is at 153 TYRWHITT ROAD QINAN BUILDING SINGAPORE 207566.')
+
+if st.button("Display 30 similar buildings"):
+        st.dataframe(df1)
+
